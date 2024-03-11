b3b34d69b561fc397f80c913111855e2ea4a9336
olihm <106966316+olihm@users.noreply.github.com>
2024-03-11 11:37:25 -0300

    .

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const botoes = document.querySelectorAll(".botao
 console.log (botoes)
 
 for (let i=0; i <botoes.length;i++) {
-botoes[i].onclick = function() {
-botoes[i].classList.add ("ativo")
+    botoes[i].onclick = function() {
 }
+
+for (letj=0; j <botoes.length; j++) {
+botoes
 }
