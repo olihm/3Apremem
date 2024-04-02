6dc989b2f326e6c5ccec56eb2f057c05bc28e32f
olihm <106966316+olihm@users.noreply.github.com>
2024-04-02 11:33:02 -0300

    .

--- main.js
+++ main.js
@@ -31,3 +31,6 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador")
 contadores[0].textContent = "Contagem regressiva";
 
+const tempoObjetivo1 = new Date("2024-10-05");
+
+contadores[0].textContent = tempoObjetivo1;
