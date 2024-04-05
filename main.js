2affb09cf8a84cfc1fe16dd23fe10df0d8431cb1
olihm <106966316+olihm@users.noreply.github.com>
2024-04-05 08:37:49 -0300

    .

--- main.js
+++ main.js
@@ -33,3 +33,6 @@ contadores[0].textContent = "Contagem regressiva";
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
 contadores[0].textContent = tempoObjetivo1;
+
+let tempoAtual = new Date ();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
