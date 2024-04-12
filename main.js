b05746c246ba77f62fece02e01832d577ffb7e87
olihm <106966316+olihm@users.noreply.github.com>
2024-04-12 08:33:45 -0300

    .

--- main.js
+++ main.js
@@ -48,3 +48,12 @@ let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
 
 return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+.contador{
+    font-size: 20px;
+    text-align: center;
+}
