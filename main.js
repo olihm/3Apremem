a59d55e5e6f1d202815c75b9ddc19ff0cf74f889
olihm <106966316+olihm@users.noreply.github.com>
2024-05-03 08:23:37 -0300

    .

--- main.js
+++ main.js
@@ -17,8 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-20T09:00:00");
 const tempoObjetivo2 = new Date("2024-10-27T09:30:00");
-const tempoObjetivo3 = new Date("2024-05-01T18:30:00");
-const tempoObjetivo4 = new Date("2025-02-01T18:00:00");
+const tempoObjetivo3 = new Date("2024-06-01T18:30:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
